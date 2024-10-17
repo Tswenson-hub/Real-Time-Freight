7f09fa36370a2ce51b5df917b4be4850a4d4f442
Ty Swenson <tyswenson34@gmail.com>
2024-10-17 14:47:15 -0500

    Navbar recolor

--- src/navbar.js
+++ src/navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ session }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           We Teach Freight
